a93b3a32c5a793c56c5a2f5fffa2ed7742427915
Zack Lee <Zack Lee>
2018-11-18 09:49:09 +0800

    add lang exception dir

--- Public/index.php
+++ Public/index.php
@@ -3,10 +3,11 @@
 const APP_ENV = 'DEVELOPMENT'; //开发环境配置
 //const APP_ENV = 'PRODUCTION'; //生产环境配置
 
+//php7+ nginx conf: {fastcgi.conf} remove line {open_basedir...}
+
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_PATH', realpath(dirname(__FILE__) . DS . '..' . DS) . DS);
-//fastcgi.conf  open_basedir remove on nginx conf
 require_once(ROOT_PATH . 'Strawframework' . DS . 'Common' . DS . 'Main.php');
 
 //Run
-Strawframework\Common\Main::getInstance()->setEnv(APP_ENV)->configure()->run();
+Strawframework\Common\Main::getInstance()->setEnv(APP_ENV)->configure('Straw')->run();
