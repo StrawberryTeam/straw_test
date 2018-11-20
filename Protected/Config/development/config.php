046c08f5c3006c48b4444cf62ba7650d85d94542
Zack Lee <zacklees@hotmail.com>
2018-11-20 14:37:34 +0800

    new config loader

--- Protected/Config/development/config.php
+++ Protected/Config/development/config.php
@@ -7,17 +7,22 @@
 return [
     //winterspring
     // 'api_key' => '230d2f705f9281e82e6b6fad903835eb',
-    'site_name' => '卡通世界',
+    'site_name' => 'StrawTest',
     //网站主域名
     'site_domain' => 'zlizhe.com',
     //首选语言包 - => _
     'default_lang' => 'zh_CN',
     //本应用 module
     'module_name' => 'pi',
-    //default database 留空则不使用不加载数据库配置
+    //default database
     'database' => 'mongo',
-    //是否启用 cache  留空则不使用不加载cache配置
+    //是否启用 cache 
     'cache' => 'redis',
     //默认缓存时间 5min = 300
     'cache_expire' => 0,
+    //加载其他配置文件
+    'ext' => [
+        'databases',
+        'caches',
+    ]
 ];
